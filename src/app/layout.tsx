91994aa39a978d94276b3dd332d05fabd1cb210c
Mystik <65687597+Mystik01@users.noreply.github.com>
2025-11-06 23:25:26 +0000

    update <Analytics />

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,6 +97,7 @@ export default async function RootLayout({
           <Flex zIndex={0} fillWidth padding="l" horizontal="center" flex={1}>
             <Flex horizontal="center" fillWidth minHeight="0">
               {children}
+              <Analytics />
             </Flex>
           </Flex>
           <Footer />
